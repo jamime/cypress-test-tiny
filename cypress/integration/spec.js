b558792a555790a356d419cd4434a87477d87ee5
Jamie Thompson <jamie.thompson@sage.com>
2022-01-04 12:51:39 +0000

    test: add click testcase

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+it("triggers click event", () => {
+  cy.visit("./index.html");
+  cy.get("#target").click();
+  cy.get("#target").type("{enter}");
+  cy.get("li").eq(0).should("have.text", "click");
+  cy.get("li").eq(1).should("have.text", "keydown");
+  cy.get("li").eq(2).should("have.text", "keypress");
+  cy.get("li").eq(3).should("have.text", "click");
+  cy.get("li").eq(4).should("have.text", "keyup");
+});
